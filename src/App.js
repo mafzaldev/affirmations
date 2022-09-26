29fb4390392d1695a128ce9cd13fa5fe58ebdbd9
mafzaldev <afzalashraf814@gmail.com>
2022-09-26 15:10:52 +0500

    integrated soundcloud

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 function App() {
-  const colors = ["#ffce0b", "#e83f68", "#ab3e8f", "#0092d6", "#00a989"];
+  const colors = [
+    "#ffce0b",
+    "#e83f68",
+    "#f941cc",
+    "#ab3e8f",
+    "#0080c4",
+    "#0092d6",
+    "#00a989",
+    "#ffce0b",
+    "#5e4e9c",
+  ];
   const [index, setIndex] = useState(0);
   const [currentAff, setCurrentAff] = useState(0);
   const [affirmations, setAffirmations] = useState([
@@ -12,10 +22,12 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex((prev) => (prev + 1) % 5);
+      setIndex((prev) => (prev + 1) % colors.length);
       setCurrentAff((prev) => prev + 1);
     }, 5000);
     return () => clearInterval(interval);
+
+    // eslint-disable-next-line
   }, [index]);
 
   useEffect(() => {
@@ -31,9 +43,21 @@ function App() {
 
   return (
     <div className="App" style={{ backgroundColor: colors[index] }}>
-      <div key={uuidv4()} className="affirmation-text">
-        {affirmations[currentAff].phrase}
+      <div className="affirmation">
+        <div key={uuidv4()} className="affirmation-text">
+          {affirmations[currentAff].phrase}
+        </div>
       </div>
+
+      <iframe
+        title="Soundcloud Player"
+        width="20%"
+        height="20%"
+        scrolling="no"
+        frameborder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/805331569&color=%23ff5500&auto_play=true&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=false"
+      ></iframe>
     </div>
   );
 }
