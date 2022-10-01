900c03fc7ec0701d78bcc81b24a54fc77b71643e
mafzaldev <afzalashraf814@gmail.com>
2022-10-01 13:50:42 +0500

    formatted text and fixed iframe issue

--- src/App.js
+++ src/App.js
@@ -4,21 +4,46 @@ import "./App.css";
 
 function App() {
   const colors = [
-    "#ffce0b",
-    "#e83f68",
-    "#f941cc",
-    "#ab3e8f",
+    "#5e4e9c",
     "#0080c4",
     "#0092d6",
     "#00a989",
     "#ffce0b",
-    "#5e4e9c",
+    "#f941cc",
+    "#ab3e8f",
+    "#e83f68",
   ];
+
   const [index, setIndex] = useState(0);
   const [currentAff, setCurrentAff] = useState(0);
   const [affirmations, setAffirmations] = useState([
     "Everything has cracks - that's how the light gets in",
   ]);
+
+  const shuffleAffirmations = (array) => {
+    let m = array.length,
+      t,
+      i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+  };
+
+  useEffect(() => {
+    const getAffirmations = async () => {
+      let response = await fetch(
+        "https://dulce-affirmations-api.herokuapp.com/affirmation/index"
+      );
+      let tempAffirmations = await response.json();
+      let shuffledAffirmations = shuffleAffirmations(tempAffirmations);
+      setAffirmations(shuffledAffirmations);
+    };
+    getAffirmations();
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -30,17 +55,6 @@ function App() {
     // eslint-disable-next-line
   }, [index]);
 
-  useEffect(() => {
-    const getAffirmations = async () => {
-      let response = await fetch(
-        "https://dulce-affirmations-api.herokuapp.com/affirmation/index"
-      );
-      let tempAffirmations = await response.json();
-      setAffirmations(tempAffirmations);
-    };
-    getAffirmations();
-  }, []);
-
   return (
     <div className="App" style={{ backgroundColor: colors[index] }}>
       <div className="affirmation">
@@ -48,13 +62,12 @@ function App() {
           {affirmations[currentAff].phrase}
         </div>
       </div>
-
       <iframe
         title="Soundcloud Player"
-        width="20%"
+        width="25%"
         height="20%"
         scrolling="no"
-        frameborder="no"
+        frameBorder="no"
         allow="autoplay"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/805331569&color=%23ff5500&auto_play=true&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=false"
       ></iframe>
